a81ee44ca6db4d9f6d7b922985946e9092908f3a
zcong1993 <zhangcong1992@gmail.com>
2020-04-29 10:54:00 +0800

    feat: deploy to now

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { getAll } = require('./api')
 
 const run = async () => {
   const config = await getAll()
-  fs.writeFileSync('./config/jav.json', JSON.stringify(config))
+  fs.writeFileSync('./public/jav.json', JSON.stringify(config))
 }
 
 run()
